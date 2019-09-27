b2d1432a9f54db3527782ba481aa8baefed4f3bd
Ralphie Celedon <ralphie@Ralphies-MacBook-Pro.local>
2019-09-27 13:18:27 -0700

    fixed get scream, styled cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme"
 import Navbar from "./components/Navbar";
 
 // Pages
@@ -8,21 +10,40 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#33ab9f",
+      main: "#009688",
+      dark: "#00695f",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#4aedc4",
+      main: "#1de9b6",
+      dark: "#14a37f",
+      contrastText: "#fff"
+    }
+  }
+})
+
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        <Router>
-        <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={home}/>
-              <Route exact path="/login" component={login}/>
-              <Route exact path="/signup" component={signup}/>
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+          <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home}/>
+                <Route exact path="/login" component={login}/>
+                <Route exact path="/signup" component={signup}/>
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
