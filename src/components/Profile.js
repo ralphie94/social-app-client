6ad8f09279ec28488f5acba12d05a0de60f679a5
Ralphie Celedon <ralphie94@pacbell.net>
2020-01-19 22:21:54 -0800

    edited navbar buttons

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,6 +4,7 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import EditDetails from "../components/EditDetails";
+import MyButton from "../util/MyButton";
 
 // MUI Stuff
 import Button from "@material-ui/core/Button";
@@ -100,11 +101,9 @@ class Profile extends Component {
                             hidden="hidden"
                             onChange={this.handleImageChange}
                         />
-                        <Tooltip title="Edit profile picture" placement="top">
-                            <IconButton onClick={this.handleEditPicture} className="button">
-                                <EditIcon color="primary"></EditIcon>
-                            </IconButton>
-                        </Tooltip>
+                        <MyButton tip="Edit profile picture" onClick={this.handleEditPicture} btnClassName="button">
+                            <EditIcon color="primary"/>
+                        </MyButton>
                     </div>
                     <hr/>
                     <div className="profile-details">
@@ -132,11 +131,9 @@ class Profile extends Component {
                         <CalendarToday color="primary"/>{" "}
                         <span>Joined {dayjs(createdAt).format("MMM YYYY")}</span>
                     </div>
-                    <Tooltip title="Logout" placement="top">
-                            <IconButton onClick={this.handleLogout}>
-                                <KeyboardReturn color="primary"/>
-                            </IconButton>
-                    </Tooltip>
+                    <MyButton tip="Logout" onClick={this.handleLogout}>
+                            <KeyboardReturn color="primary"/>
+                        </MyButton>
                     <EditDetails/>
                 </div>
             </Paper>
