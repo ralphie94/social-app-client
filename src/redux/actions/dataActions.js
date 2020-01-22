4cf1fcfe3c74094f92b39fef265b64f8b80ea57a
Ralphie Celedon <ralphie94@pacbell.net>
2020-01-21 20:45:07 -0800

    created delete scream action, built out delete scream component

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -1,4 +1,4 @@
-import { SET_SCREAMS, LOADING_DATA, LIKE_SCREAM, UNLIKE_SCREAM } from "../types";
+import { SET_SCREAMS, LOADING_DATA, LIKE_SCREAM, UNLIKE_SCREAM, DELETE_SCREAM } from "../types";
 import axios from "axios";
 import { log } from "util";
 
@@ -40,6 +40,14 @@ export const unlikeScream = (screamId) => (dispatch) => {
                 type: UNLIKE_SCREAM,
                 payload: res.data
             })
+        })
+        .catch(err => console.log(err));
+};
+
+export const deleteScream = (screamId) => (dispatch) => {
+    axios.delete(`/scream/${screamId}`)
+        .then(() => {
+            dispatch({ type: DELETE_SCREAM, payload: screamId })
         })
         .catch(err => console.log(err));
 }
