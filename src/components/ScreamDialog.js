c437bd3456ed2f7ef82d24093b1d98368d1ef6e0
Ralphie Celedon <ralphie94@pacbell.net>
2020-01-22 23:17:25 -0800

    fixed styling in expand post modal

--- src/components/ScreamDialog.js
+++ src/components/ScreamDialog.js
@@ -70,6 +70,19 @@ const styles = theme => ({
     invisibleSeparator: {
         border: "none",
         margin: 4
+    },
+    profileImage: {
+        maxWidth: 200,
+        height: 200,
+        borderRadius: "50%",
+        objectFit: "cover"
+    },
+    dialogContent: {
+        padding: 20
+    },
+    closeButton: {
+        position: 'absolute',
+        left: '90%'
     }
 });
 
@@ -146,7 +159,7 @@ class ScreamDialog extends Component {
                 >
                     <CloseIcon />
                 </MyButton>
-                <DialogContent className={classes.DialogContent}>
+                <DialogContent className={classes.dialogContent}>
                     {dialogMarkup}
                 </DialogContent>
                 </Dialog>
