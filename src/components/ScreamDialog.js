014d6fb97c863b668d86b118f2165f7ba7bb3792
Ralphie Celedon <ralphie94@pacbell.net>
2020-01-23 14:16:36 -0800

    added like button and comments in expand post modal

--- src/components/ScreamDialog.js
+++ src/components/ScreamDialog.js
@@ -2,13 +2,13 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 import MyButton from "../util/MyButton";
+import LikeButton from './LikeButton';
 import dayjs from "dayjs";
 import { Link } from "react-router-dom";
 
 // MUI
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
-import DialogTitle from "@material-ui/core/DialogTitle";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
@@ -16,6 +16,7 @@ import Typography from "@material-ui/core/Typography";
 // Icons
 import CloseIcon from "@material-ui/icons/Close";
 import UnfoldMore from "@material-ui/icons/UnfoldMore";
+import ChatIcon from "@material-ui/icons/Chat";
 
 // Redux
 import { connect } from "react-redux";
@@ -148,6 +149,12 @@ class ScreamDialog extends Component {
                     </Typography>
                     <hr className={classes.invisibleSeparator} />
                     <Typography variant="body1">{body}</Typography>
+                    <LikeButton screamId={screamId}/>
+                    <span>{likeCount} Likes</span>
+                    <MyButton tip="comments">
+                        <ChatIcon color="primary"/>
+                    </MyButton>
+                    <span>{commentCount} Comments</span>
                 </Grid>
             </Grid>
         )
